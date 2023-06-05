e84fec4578d52f71fda0694ca3b64c2df574d6f0
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-06-05 15:18:14 +0300

    add favicon

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Schedule />} />
+          <Route path="/train-timetable-manager" element={<Schedule />} />
           <Route path="/add" element={<AddTimeTable />} />
           <Route path="/update/:id" element={<UpdateTimeTable />} />
         </Routes>
